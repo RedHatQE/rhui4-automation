c7b050c1194690409ee8da57c4c7c2620c48b0c8
Radek Bíba <radek@redhat.com>
2022-07-29 09:26:49 +0200

    add orphan_protection_time to allow even fresh orphans to be deleted in new pulpcore

--- tests/rhui4_tests_lib/pulp_api.py
+++ tests/rhui4_tests_lib/pulp_api.py
@@ -8,10 +8,11 @@ from rhui4_tests_lib.util import Util
 class PulpAPI():
     """ Pulp API functions """
     @staticmethod
-    def delete_orphans(connection):
+    def delete_orphans(connection, orphan_protection_time=0):
         """ delete all orphaned content """
         admin_password = Util.get_saved_password(connection)
         rhua_hostname = ConMgr.get_rhua_hostname()
         Expect.expect_retval(connection,
                              f"curl --cacert /etc/pki/rhui/certs/ca.crt -u admin:{admin_password}" +
+                             f" -d orphan_protection_time={orphan_protection_time}" +
                              f" -X POST https://{rhua_hostname}/pulp/api/v3/orphans/cleanup/")
